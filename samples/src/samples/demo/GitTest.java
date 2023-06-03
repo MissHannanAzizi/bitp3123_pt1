42ebe98704adb43ba350ae794c8f48557c8260e8
Hannan Azizi <hannanazizi@gmail.com>
2023-06-03 13:09:16 +0800

    Initial Commit

--- samples/src/samples/demo/GitTest.java
+++ samples/src/samples/demo/GitTest.java
@@ -4,6 +4,8 @@ public class GitTest {
 
     public static void main(String[] args) {
         System.out.println("Git is Fun");
+        //new implementation
+        System.out.println("Git is cool");
     }
 	
 }
